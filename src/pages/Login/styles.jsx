f0f6a64d3e37e91596d1e4ec752a3ba06badf234
babislz <gabriela.laureano2007@gmail.com>
2024-07-16 15:09:02 -0300

    background image

--- src/pages/Login/styles.jsx
+++ src/pages/Login/styles.jsx
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 const Background = styled.img `
     width: 100%;
-    height: 100%;
+    height: inherit;
+    object-fit: cover;
+    overflow: hidden;
 `;
 
 export { Background };
