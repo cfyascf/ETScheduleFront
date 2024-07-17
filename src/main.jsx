6c6cc74aa475db772f722897a3cd3cf1862f0aff
Mariasina <carolbeveer@gmail.com>
2024-07-17 08:11:06 -0300

    merged feat-mari with main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import mainRoutes from './routes/mainRoutes.jsx'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
