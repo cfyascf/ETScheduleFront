87c93db89275e29e6600e07fef4a4238ec6628b2
babislz <gabriela.laureano2007@gmail.com>
2024-07-18 08:24:08 -0300

    updated routes

--- src/routes/mainRoutes.jsx
+++ src/routes/mainRoutes.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import Courses from "../pages/Courses";
 import CoursesReg from "../pages/CoursesRegister"
-import CoursesReg from "../pages/";
 import Home from "../pages/Home"
 
 const mainRoutes = createBrowserRouter([
