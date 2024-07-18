8066c6aabc7089c930292597e8f278cea4c4ec00
babislz <gabriela.laureano2007@gmail.com>
2024-07-18 13:10:10 -0300

    fixed little little little bugs

--- src/pages/Courses/index.jsx
+++ src/pages/Courses/index.jsx
@@ -5,7 +5,7 @@ import SideProfileClose from "../../components/SideProfileClose"
 import SideProfileOpen from "../../components/SideProfileOpen"
 import StudentMenu from "../../components/StudentMenu";
 import Card from "../../components/Card/cards";
-import { MainContainer, PageContent, CardsContainer, PageContentItems, TopContent } from "./styles";
+import { PageContent, CardsContainer, PageContentItems, TopContent } from "./styles";
 
 const coursesData = [
     { course: 'Python', name: 'Donathan Ramalho', semester: '2', color: '#fcba03' },
@@ -16,11 +16,11 @@ const coursesData = [
     { course: 'IoT', name: 'Queila Lima', semester: '1', color: '#8cc8d1' },
     { course: 'Comunicação', name: 'Queila Lima', semester: '2', color: 'green' },
     { course: 'Inglês', name: 'Queila Lima', semester: '2', color: 'brown' },
-    { course: 'IA', name: 'Leonardo Trevisan', semester: '3', color: 'aqua' }
+    { course: 'IA', name: 'Leonardo Trevisan', semester: '3', color: 'aqua' },
     { course: 'Python', name: 'Donathan Ramalho' },
     { course: 'Power BI', name: 'Donathan Ramalho' },
     { course: 'Java', name: 'Leonardo Trevisan' },
-    { course: 'Java Avançado', name: 'Leonardo Trevisan' },
+    { course: 'Java Avançado', name: 'Leonardo Trevisan'},
     { course: 'C#', name: 'Leonardo Trevisan' },
     { course: 'IoT', name: 'Queila Lima' },
     { course: 'Comunicação', name: 'Queila Lima' },
