0d9ce2d6798aa73b716fc96288fbe3d4ed2d8d81
babislz <gabriela.laureano2007@gmail.com>
2024-07-18 13:08:58 -0300

    merged feat-gabi

--- src/pages/Courses/index.jsx
+++ src/pages/Courses/index.jsx
@@ -8,15 +8,15 @@ import Card from "../../components/Card/cards";
 import { MainContainer, PageContent, CardsContainer, PageContentItems, TopContent } from "./styles";
 
 const coursesData = [
-    {course: 'Python', name: 'Donathan Ramalho', semester: '2', color: '#fcba03'},
-    {course: 'Power BI', name: 'Donathan Ramalho', semester: '2', color: '#6b03fc'},
-    {course: 'Java', name: 'Leonardo Trevisan', semester: '2', color: 'purple'},
-    {course: 'Java Avançado', name: 'Leonardo Trevisan', semester: '1', color: '#fc7303'},
-    {course: 'C#', name: 'Leonardo Trevisan', semester: '1', color: '#0384fc'},
-    {course: 'IoT', name: 'Queila Lima', semester: '1', color: '#8cc8d1'},
-    {course: 'Comunicação', name: 'Queila Lima', semester: '2', color: 'green'},
-    {course: 'Inglês', name: 'Queila Lima', semester: '2', color: 'brown'},
-    {course: 'IA', name: 'Leonardo Trevisan', semester: '3', color: 'aqua'}
+    { course: 'Python', name: 'Donathan Ramalho', semester: '2', color: '#fcba03' },
+    { course: 'Power BI', name: 'Donathan Ramalho', semester: '2', color: '#6b03fc' },
+    { course: 'Java', name: 'Leonardo Trevisan', semester: '2', color: 'purple' },
+    { course: 'Java Avançado', name: 'Leonardo Trevisan', semester: '1', color: '#fc7303' },
+    { course: 'C#', name: 'Leonardo Trevisan', semester: '1', color: '#0384fc' },
+    { course: 'IoT', name: 'Queila Lima', semester: '1', color: '#8cc8d1' },
+    { course: 'Comunicação', name: 'Queila Lima', semester: '2', color: 'green' },
+    { course: 'Inglês', name: 'Queila Lima', semester: '2', color: 'brown' },
+    { course: 'IA', name: 'Leonardo Trevisan', semester: '3', color: 'aqua' }
     { course: 'Python', name: 'Donathan Ramalho' },
     { course: 'Power BI', name: 'Donathan Ramalho' },
     { course: 'Java', name: 'Leonardo Trevisan' },
@@ -45,18 +45,20 @@ const Courses = () => {
                 )}
                 <TopContent>
                     <StudentMenu />
-                    <CardsContainer>
-                        {coursesData.map((courseData, index) => (
-                            <Card 
-                                key={index} 
-                                course={courseData.course} 
-                                name={courseData.name}
-                                semester={courseData.semester} 
-                                color={courseData.color} 
-                            />
-                        ))}
-                    </CardsContainer>
-                </PageContentItems>
+                    <PageContentItems>
+                        <CardsContainer>
+                            {coursesData.map((courseData, index) => (
+                                <Card
+                                    key={index}
+                                    course={courseData.course}
+                                    name={courseData.name}
+                                    semester={courseData.semester}
+                                    color={courseData.color}
+                                />
+                            ))}
+                        </CardsContainer>
+                    </PageContentItems>
+                </TopContent>
             </PageContent>
             <Footer />
         </>
