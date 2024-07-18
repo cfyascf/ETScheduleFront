1e47b1b4f4817cf1a80f6261393e54c7e0445a1d
Mariasina <carolbeveer@gmail.com>
2024-07-18 16:50:18 -0300

    working on student page

---
+++ src/pages/Student/components/StudentGraph/styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const GraphContainer = styled.div`
+    background-color: turquoise;
+    width: 50%;
+    height: 400px;
+    display: flex;
+`
+
+export {GraphContainer}
---
+++ src/pages/Student/components/StudentHeader/styles.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StudentContainer = styled.div`
+    background-color: #F0F0F0;
+    padding: 15px 25px;
+    display: flex;
+    align-items: center;
+    gap: 30px;
+    font-size: 1.3rem;
+`
+
+const StudentIcon = styled.img`
+    width: 60px;
+`
+
+export {StudentContainer, StudentIcon}
