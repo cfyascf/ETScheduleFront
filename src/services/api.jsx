9fb5e7f1785f96b453cd6d332447e70c1686c8e5
yasmi <yas1cf1@gmail.com>
2024-07-19 16:18:12 -0300

    mergin yasmin

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,6 +2,10 @@ import axios from "axios"
 
 const api = axios.create({
     baseURL: 'http://localhost:8080/api/v1',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('@AUTH')
+    }
 })
 
 export default api;
