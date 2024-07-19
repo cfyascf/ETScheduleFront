2ce71d315b83392fe36c8053ea48cdb6940110d5
dayne pacheco <daynepacheco@gmail.com>
2024-07-19 14:50:34 -0300

    vendo os graficos

--- src/components/GraphicRadarHard/GraphicRadarHard.jsx
+++ src/components/GraphicRadarHard/GraphicRadarHard.jsx
@@ -49,7 +49,7 @@ const GraphicRadarHard = () => {
                   <Legend ></Legend>
                   <PolarGrid />
                   <PolarAngleAxis dataKey="subject" />
-                  <PolarRadiusAxis angle={30} domain={[0, 100]} />
+                  <PolarRadiusAxis angle={30} domain={[0, 150]} />
                   <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
                   <Radar name="Lily" dataKey="B" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6} />
               </RadarChart>
