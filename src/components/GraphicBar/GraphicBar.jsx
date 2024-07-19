08eca9288645940b6e70704d890cee75a1604608
YasminTrembulack <yasmin.agostinho012@gmail.com>
2024-07-19 16:12:09 -0300

    feat: implementing personal report integration

--- src/components/GraphicBar/GraphicBar.jsx
+++ src/components/GraphicBar/GraphicBar.jsx
@@ -1,57 +1,62 @@
-import { GraphicContainer, Title} from "./styles";
-import { Legend, BarChart,Bar,Tooltip,YAxis,XAxis,CartesianGrid } from 'recharts';
-
-const data = [
-  {
-    "name": "Page A",
-    "uv": 4000,
-    "pv": 2400
-  },
-  {
-    "name": "Page B",
-    "uv": 3000,
-    "pv": 1398
-  },
-  {
-    "name": "Page C",
-    "uv": 2000,
-    "pv": 9800
-  },
-  {
-    "name": "Page D",
-    "uv": 2780,
-    "pv": 3908
-  },
-  {
-    "name": "Page E",
-    "uv": 1890,
-    "pv": 4800
-  },
-  {
-    "name": "Page F",
-    "uv": 2390,
-    "pv": 3800
-  },
-  {
-    "name": "Page G",
-    "uv": 3490,
-    "pv": 4300
-  }
-]
-
+import { GraphicContainer, Title } from "./styles";
+import { useEffect, useState } from "react"
+import { Legend, BarChart, Bar, Tooltip, YAxis, XAxis, CartesianGrid } from 'recharts';
+import api from "../../services/api"
 
 const GraphicBar = () => {
+
+  const mock = [
+    { discipline: 'Python', grade: 7.0 },
+    { discipline: 'Java', grade: 7.0 },
+    { discipline: 'Web', grade: 7.0 },
+    { discipline: 'IoT', grade: 7.0 }
+  ];
+  
+  const [data, setDataB] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const userInfo = parseJwt()
+
+        const response = await api.get(`/report/profile/${userInfo['profileId']}`)
+
+        const performances = response.data['performances'];
+
+
+        const data_b_dict = performances.map(u => {
+          return { discipline: u['discipline'], grade: u['average'] };
+        });
+
+        setDataB(data_b_dict)
+
+      } catch (error) {
+        setDataB(mock);
+      }
+    }
+    getData();
+  }, [])
+
+  function parseJwt() {
+    var base64Url = localStorage.getItem('@AUTH').split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+  }
+
   return (
     <>
       <GraphicContainer>
         <Title>Average of Subject</Title>
-        <BarChart width={730} height={250} data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+        <BarChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+          <Bar dataKey="grade" fill="#1adb00" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="discipline" />
+          <YAxis domain={[0, 10.0]} />
           <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" fill="#82ca9d" />
         </BarChart>
       </GraphicContainer>
     </>
